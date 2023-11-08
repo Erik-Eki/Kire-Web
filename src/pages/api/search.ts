b05f9857012652b844f8cfa83f7cbde6d0bdd983
Erik <eriko.oy38@gmail.com>
2023-11-08 14:49:31 +0200

    fixed search, again...

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -10,7 +10,7 @@ async function searchSite(query) {
     .from('posts')
     .select('*')
     .or(`title.ilike.%${query}%,description.ilike.%${query}%`)
-    // .textSearch('title', query);
+  // .textSearch('title', query);
 
   if (error) throw error;
 
@@ -18,32 +18,50 @@ async function searchSite(query) {
 }
 
 export async function GET({ params, request }: APIContext) {
-//export default async (req, res) => {
-    const query = new URL(request.url).searchParams
-    const body = escapeRegExp(String(query.getAll("q")))
-    //const query = request.query.q;
-  
+  //export default async (req, res) => {
+  const query = new URL(request.url).searchParams
+  const body = escapeRegExp(String(query.getAll("q")))
+  //const query = request.query.q;
+
+  if (body === "null") {
+    return new Response(null)
+  }
+  else {
+
     // Your search logic here. This could be a database search,
     // a search in your CMS, a call to a third-party search API, etc.
     // This will depend on how your site's data is structured.
     const searchResult = await searchSite(body)
 
+    console.log(searchResult)
 
-    let results = '<h3>Search results:</h3><div class="border-b border-b-white">';
-    // Loop through the data and create a new div for each result
-    searchResult.forEach(i => {
-      const highlightedTitle = i.title.replace(new RegExp(body, 'gi'), (match) => `<mark>${match}</mark>`);
-      const highlightedDescription = i.description.replace(new RegExp(body, 'gi'), (match) => `<mark>${match}</mark>`);
+    let results = '<h3>Search results:</h3><div';
 
-      results += `
+    if (searchResult.length > 0) {
+      // Loop through the data and create a new div for each result
+      searchResult.forEach(i => {
+        const highlightedTitle = i.title.replace(new RegExp(body, 'gi'), (match) => `<mark>${match}</mark>`);
+        const highlightedDescription = i.description.replace(new RegExp(body, 'gi'), (match) => `<mark>${match}</mark>`);
+
+        results += `
         <h2 class=" p-0">
           <a href="${i.slug}">${highlightedTitle}</a>
         </h2>
         <i>${highlightedDescription}</i>
         <hr/>
       `;
-    });
-  
+      });
+    }
+    else {
+      results += `
+        <h2 class=" p-0">
+          No matches in titles or descriptions...
+        </h2>
+        <hr/>
+      `;
+    }
+
     //res.status(200).send(results);<div class="border-b border-b-white">
     return new Response(results);
-  };
+  }
+};
